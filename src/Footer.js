10a334b380637084f2b709ea74a5498f9e6259bd
dkahmad95 <dkahmad95@gmail.com>
2023-09-20 20:58:50 +0300

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = ({ length }) => {
+  return (
+    <footer>
+      <p>{length} List {length === 1 || length === 0 ? "item" : "items"} </p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/content.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import ItemList from './ItemList';
+
+
+const Content = ({ items, handleCheck, handleDelete }) => {
+
+
+    return (
+        <>
+            {items.length ? (
+                <ItemList
+                    items={items}
+                    handleCheck={handleCheck}
+                    handleDelete={handleDelete}
+                />
+            ) : (
+                <p style={{ marginTop: '2rem' }}>
+                    YOUR LIST IS EMPTY.
+                </p>
+            )}
+        </>
+
+    )
+}
+
+export default Content
